311b37895c68b77e56280dfd2942952eefaad8fc
johnfernandis1999 <140940560+johnfernandis1999@users.noreply.github.com>
2024-09-12 18:33:04 +0530

    File added

---
+++ useState/src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+import Color from './components/example1/Color'
+
+function App() {
+
+  return (
+   <>
+     <Color/>
+   </>
+  )
+}
+
+export default App
---
+++ useState/src/components/example1/Color.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react"
+
+
+function Color() {
+
+  const [color ,setColor] = useState("Red")
+
+
+  const handleChange = () =>{
+    setColor ((prev) =>( prev === "Red" ? "Blue" : "Red"))
+  }
+  return (
+    <div>
+       <h1>This Color is {color}</h1>
+       <button onClick={handleChange}>{color === "Red" ? "Blue":"Red"}</button>
+    </div>
+  )
+}
+
+export default Color;
